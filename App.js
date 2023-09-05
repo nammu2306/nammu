64457f08038ec97ac12959916ceb865db2f1e117
nammu2306 <144111737+nammu2306@users.noreply.github.com>
2023-09-05 16:19:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <body>
+
+<div class="header">
+  <h1>Namratha M</h1>
+  <p>Resize the browser window to see the responsive effect.</p>
+  <body>
+  <ul>
+  <li><a href="https://www.bing.com/search?q=What+is+react+JS%3f&FORM=HDRSC1">Home</a></li>
+  <li><a href="https://www.bing.com/news/search?q=What+is+react+JS%3f&FORM=HDRSC7">News</a></li>
+  <li><a href="https://www.bing.com/images/search?q=What+is+react+JS%3f&form=HDRSC3&first=1">About</a></li>
+</ul>
+  </body>
+</div>
+
+<div class="topnav">
+  <a href="https://www.w3schools.com/css/css_website_layout.asp">Learn css</a>
+  <a href="https://www.w3schools.com/react/default.asp">Lear react</a>
+  <a href="https://www.w3schools.com/nodejs/default.asp">Lear nodejs</a>
+</div>
+
+<div class="row">
+  <div class="column">
+    <h2>CSS</h2>
+    <p>CSS stands for Cascading Style Sheets
+CSS describes how HTML elements are to be displayed on screen, paper, or in other media
+CSS saves a lot of work. It can control the layout of multiple web pages all at once
+External stylesheets are stored in CSS files
+</p>
+  </div>
+  
+  <div class="column">
+    <h2>REACT</h2>
+    <p>React, sometimes referred to as a frontend JavaScript framework, is a JavaScript library created by Facebook.
+
+React is a tool for building UI components.
+
+How does React Work?
+React creates a VIRTUAL DOM in memory.
+
+Instead of manipulating the browser's DOM directly, React creates a virtual DOM in memory, where it does all the necessary manipulating, before making the changes in the browser DOM.</p>
+  </div>
+  
+  <div class="column">
+    <h2>NODEJS</h2>
+    <p>Node.js is an open source server environment
+Node.js is free
+Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)
+Node.js uses JavaScript on the server</p>
+  </div>
+</div>
+
+
+</body>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
